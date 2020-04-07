cfff8fd292c1ff8da3681ec7dffa0fa115ee700f
Piper Merriam <pipermerriam@gmail.com>
2020-04-07 10:13:15 -0600

    d

--- alexandria/app.py
+++ alexandria/app.py
@@ -72,16 +72,15 @@ class Application(Service):
         )
 
     async def run(self) -> None:
-        with trio.fail_after(5):
-            async with self.client.events.listening.subscribe():
-                self.manager.run_daemon_child_service(self.client)
+        self.manager.run_daemon_child_service(self.client)
+        self.manager.run_daemon_task(self._monitor_endpoints)
 
-        await self.client.wait_ready()
+        with trio.fail_after(5):
+            await self.client.wait_ready()
         self.manager.run_task(self._bootstrap)
         await self._bonded.wait()
         self.logger.info("Bonding Successful!!!")
 
-        self.manager.run_daemon_task(self._monitor_endpoints)
         self.manager.run_daemon_child_service(self.kademlia)
 
         await self.manager.wait_finished()
